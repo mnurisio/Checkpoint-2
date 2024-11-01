cfee7dbb8aff4769d28ce6e515e79d919d1fbd14
Mike Nurisio <michaelnurisio@gmail.com>
2024-11-01 14:03:48 -0600

    consolidated click upgrades to one buy function

--- app.js
+++ app.js
@@ -4,6 +4,9 @@
 //#region State
 
 let notes = 0
+let clickModifier = 1
+let autoModifier = 0
+
 
 let clickUpgrades = [
     {
@@ -46,11 +49,68 @@ function mine(){
     console.log('mined notes', notes);
 }
     
-function buyEquipment(){
-    let equipment = clickUpgrades[0]
-    equipment.quantity++
-    notes -= equipment.price
-    drawEquipQuant()
+// function buyEquipment(){
+//     let equipment = clickUpgrades[0]
+//     equipment.quantity++
+//     notes -= equipment.price
+//     drawClickUpgradeQuant()
+//     clickIncrease()
+// }
+// function buyProducer(){
+//     let producer = clickUpgrades[1]
+//     producer.quantity++
+//     notes -= producer.price
+//     drawClickUpgradeQuant()
+//     clickIncrease()
+// }
+
+function buyClickUpgrade(itemName){
+    const upgrade = clickUpgrades.find(item => item.name == itemName)
+    upgrade.quantity++
+    notes -= upgrade.price
+
+    drawClickUpgradeQuant()
+    clickIncrease()
+}
+
+function buyHarrison(){
+    let harrison = autoUpgrades[0]
+    harrison.quantity++
+    notes -= harrison.price
+    drawHarrisonQuant()
+    autoIncrease()
+}
+function buyStudio(){
+    let studio = autoUpgrades[1]
+    studio.quantity++
+    notes -= studio.price
+    drawStudioQuant()
+    autoIncrease()
+
+}
+
+function clickIncrease (){
+    for(i = 0; i < clickUpgrades.length; i++){
+        let upgrade = clickUpgrades[i]
+        if(upgrade.quantity > 0){
+        clickModifier += Math.floor(upgrade.quantity * upgrade.bonus)
+        }
+        console.log('click increase', clickModifier);
+        
+    }
+    drawClickModifier()
+}
+
+function autoIncrease (){
+    
+    for(i = 0; i < autoUpgrades.length; i++){
+        let upgrade = autoUpgrades[i]
+        if(upgrade.quantity > 0){
+        autoModifier += Math.floor(upgrade.quantity * upgrade.bonus)
+        }
+    }
+    console.log('auto increase', autoModifier);
+    drawAutoModifier()
 }
 
 //#endregion Logic
@@ -62,12 +122,55 @@ function drawMine(){
     notesElem.innerText = notes.toString()
 }
 
-function drawEquipQuant (){
-    const equipQuantity = clickUpgrades[0]
-    let equipElem = document.getElementById('equipQuantity')
-    equipElem.innerText = equipQuantity.quantity
-    console.log('increase equip quant', equipQuantity.quantity, notes);
+// function drawEquipQuant (){
+//     const equipQuantity = clickUpgrades[0]
+//     let equipElem = document.getElementById('equipQuantity')
+//     equipElem.innerText = equipQuantity.quantity.toString()
     
+// }
+// function drawProducerQuant (){
+//     const ProducerQuantity = clickUpgrades[1]
+//     let ProducerElem = document.getElementById('producerQuantity')
+//     ProducerElem.innerText = ProducerQuantity.quantity.toString()
+    
+// }
+
+function drawClickUpgradeQuant(){
+    for(let i = 0; i < clickUpgrades.length; i++) {
+        const upgrade = clickUpgrades[i]
+        const upgradeElem = document.getElementById(upgrade.name + 'Quantity')
+        upgradeElem.innerHTML = upgrade.quantity.toString()
+        console.log('upgradeElm', upgradeElem);
+    }
+    
+}
+
+function drawHarrisonQuant (){
+    const HarrisonQuantity = autoUpgrades[0]
+    let HarrisonElem = document.getElementById('harrisonQuantity')
+    HarrisonElem.innerText = HarrisonQuantity.quantity.toString()
+    
+}
+function drawStudioQuant (){
+    const StudioQuantity = autoUpgrades[1]
+    let StudioElem = document.getElementById('studioQuantity')
+    StudioElem.innerText = StudioQuantity.quantity.toString()
+    
+}
+
+
+function drawClickModifier(){
+    
+    let ModElem = document.getElementById('clickModifier')
+    ModElem.innerText = clickModifier.toString()
+    console.log('click increase', clickModifier);
+    
+}
+
+function drawAutoModifier(){
+
+    let ModElem = document.getElementById('autoModifier')
+    ModElem.innerText = autoModifier.toString()
 }
 
 //#endregion Graphics
