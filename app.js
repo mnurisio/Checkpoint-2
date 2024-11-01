f005a133609cb44aeffba02dd579bbdf5d7fc3c8
Mike Nurisio <michaelnurisio@gmail.com>
2024-11-01 15:56:33 -0600

    increase price of each upgrade individually once clicked, wrote kill logic if not enough resources to buy

--- app.js
+++ app.js
@@ -43,134 +43,154 @@ let autoUpgrades = [
 
 //#region Logic
 
-function mine(){
-    // notes += calcClickModifier()
+function mine() {
+    notes += calcClickModifier()
+    drawMine()
+}
+
+function autoMine() {
     notes += calcAutoModifier()
     drawMine()
 }
 
 
-function buyClickUpgrade(itemName){
+function buyClickUpgrade(itemName) {
     const upgrade = clickUpgrades.find(item => item.name == itemName)
-    upgrade.quantity++
+
+
+    if (notes < upgrade.price) {
+        window.alert('cant buy this bro, get to clickin')
+        return
+    }
+
     notes -= upgrade.price
+    upgrade.quantity++
+    upgrade.price += Math.floor(upgrade.price * Math.random())
+
 
     drawClickUpgradeQuant()
     drawClickModifier()
+    drawMine()
+    drawClickPriceModifier()
 }
 
-function buyAutoUpgrade(itemName){
+function buyAutoUpgrade(itemName) {
     const upgrade = autoUpgrades.find(item => item.name == itemName)
-    upgrade.quantity++
+
+    if (notes < upgrade.price) {
+        window.alert('cant buy this bro, get to clickin')
+        return
+    }
+
     notes -= upgrade.price
+    upgrade.quantity++
+    upgrade.price += Math.floor(upgrade.price * Math.random())
+
 
     drawAutoUpgradeQuant()
     drawAutoModifier()
+    drawMine()
+    drawAutoPriceModifier()
 }
 
-// function buyHarrison(){
-//     let harrison = autoUpgrades[0]
-//     harrison.quantity++
-//     notes -= harrison.price
-//     drawHarrisonQuant()
-//     autoIncrease()
-// }
-// function buyStudio(){
-//     let studio = autoUpgrades[1]
-//     studio.quantity++
-//     notes -= studio.price
-//     drawStudioQuant()
-//     autoIncrease()
-
-// }
-
-
-function autoIncrease (){
-    for(i = 0; i < autoUpgrades.length; i++){
+function autoIncrease() {
+    for (let i = 0; i < autoUpgrades.length; i++) {
         let upgrade = autoUpgrades[i]
-        if(upgrade.quantity > 0){
-        autoModifier += Math.floor(upgrade.quantity * upgrade.bonus)
+        if (upgrade.quantity > 0) {
+            autoModifier += Math.floor(upgrade.quantity * upgrade.bonus)
         }
     }
-    console.log('auto increase', autoModifier);
     drawAutoModifier()
 }
 
-function calcClickModifier(){
+function calcClickModifier() {
     let clickModifier = 1
-    for(i = 0; i < clickUpgrades.length; i++){
+    for (let i = 0; i < clickUpgrades.length; i++) {
         let upgrade = clickUpgrades[i]
-        if(upgrade.quantity > 0){
-        clickModifier += Math.floor(upgrade.quantity * upgrade.bonus)
-        } 
+        if (upgrade.quantity > 0) {
+            clickModifier += Math.floor(upgrade.quantity * upgrade.bonus)
+        }
     } return clickModifier
 }
 
-function calcAutoModifier(){
+function calcAutoModifier() {
     let autoModifier = 0
-    for(i = 0; i < autoUpgrades.length; i++){
+    for (let i = 0; i < autoUpgrades.length; i++) {
         let upgrade = autoUpgrades[i]
-        if(upgrade.quantity > 0){
-        autoModifier += Math.floor(upgrade.quantity * upgrade.bonus)
+        if (upgrade.quantity > 0) {
+            autoModifier += Math.floor(upgrade.quantity * upgrade.bonus)
         }
     } return autoModifier
+}
+
+function calcClickPriceModifier() {
+    for (let i = 0; i < clickUpgrades.length; i++) {
+        let upgrade = clickUpgrades[i]
+        if (upgrade.quantity > 0) {
+            upgrade.price += Math.floor(upgrade.price * Math.random())
+        }
+        console.log('price change', upgrade.price);
+    }
+    drawClickPriceModifier()
 }
 
 //#endregion Logic
 
 //#region Graphics
 
-function drawMine(){
+function drawMine() {
     let notesElem = document.getElementById('vinyl')
     notesElem.innerText = notes.toString()
 }
 
 
-function drawClickUpgradeQuant(){
-    for(let i = 0; i < clickUpgrades.length; i++) {
+function drawClickUpgradeQuant() {
+    for (let i = 0; i < clickUpgrades.length; i++) {
         const upgrade = clickUpgrades[i]
         const upgradeElem = document.getElementById(upgrade.name + 'Quantity')
         upgradeElem.innerHTML = upgrade.quantity.toString()
-        console.log('upgradeElm', upgradeElem);
     }
-    
 }
 
-function drawAutoUpgradeQuant(){
-    for(let i = 0; i < autoUpgrades.length; i++) {
+function drawAutoUpgradeQuant() {
+    for (let i = 0; i < autoUpgrades.length; i++) {
         const upgrade = autoUpgrades[i]
         const upgradeElem = document.getElementById(upgrade.name + 'Quantity')
         upgradeElem.innerHTML = upgrade.quantity.toString()
+    }
 }
-}
 
-// function drawHarrisonQuant (){
-//     const HarrisonQuantity = autoUpgrades[0]
-//     let HarrisonElem = document.getElementById('harrisonQuantity')
-//     HarrisonElem.innerText = HarrisonQuantity.quantity.toString()
-    
-// }
-// function drawStudioQuant (){
-//     const StudioQuantity = autoUpgrades[1]
-//     let StudioElem = document.getElementById('studioQuantity')
-//     StudioElem.innerText = StudioQuantity.quantity.toString()
-// }
-
-
-function drawClickModifier(){
+function drawClickModifier() {
     let clickModifier = calcClickModifier()
     let ModElem = document.getElementById('clickModifier')
     ModElem.innerText = clickModifier.toString()
- 
+
 }
 
-function drawAutoModifier(){
+function drawAutoModifier() {
     let autoModifier = calcAutoModifier()
     let ModElem = document.getElementById('autoModifier')
     ModElem.innerText = autoModifier.toString()
     console.log('auto increase', autoModifier);
 }
 
+function drawClickPriceModifier() {
+    for (let i = 0; i < clickUpgrades.length; i++) {
+        const upgrade = clickUpgrades[i]
+        const priceElem = document.getElementById(upgrade.name + 'Price')
+        priceElem.innerHTML = upgrade.price.toString()
+    }
+}
+
+function drawAutoPriceModifier() {
+    for (let i = 0; i < autoUpgrades.length; i++) {
+        const upgrade = autoUpgrades[i]
+        const priceElem = document.getElementById(upgrade.name + 'Price')
+        priceElem.innerHTML = upgrade.price.toString()
+    }
+}
+
+setInterval(autoMine, 5000)
 //#endregion Graphics
 
 
